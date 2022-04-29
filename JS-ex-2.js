950a4d320e1d6b57959c8397e0ffee333088ad99
leoalvezmdz <alvezleonardo@hotmail.com>
2022-04-29 20:18:38 -0300

    JS ex 2

---
+++ JS-ex-2.js
@@ -0,0 +1,8 @@
+var cadena = "Esto es una cadena de texto"
+
+function miFuncion(){
+    var cadena = "Esta cadena es diferente de la primera"
+    console.log(cadena)
+}
+miFuncion()
+console.log(cadena)
